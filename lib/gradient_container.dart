c228b47c996b8942b33a21140357afceb3ada6c1
Isabel Agbu <isabelxcoding@gmail.com>
2025-01-17 23:08:18 -0800

    finished app

---
+++ lib/gradient_container.dart
@@ -0,0 +1,30 @@
+import 'package:first_app/dice_roller.dart';
+import 'package:flutter/material.dart';
+import 'package:first_app/styled_text.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.colors,{super.key});
+
+  final List<Color> colors;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: startAlignment,
+          end: endAlignment,
+          colors: colors,
+        ),
+      ),
+      child: Center(
+        child: DiceRoller()
+      ),
+    );
+  }
+}
+ 
